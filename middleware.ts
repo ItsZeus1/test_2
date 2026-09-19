b3d0b6f5fcb37f632da5868ac36a8b42183cc1c2
Majd Mohmad Awni El Madani <106382063+ItsZeus1@users.noreply.github.com>
2026-09-19 08:03:11 +0300

    ff

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
-import { createServerClient } from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from "next/server";
+
 
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({ request });
@@ -12,7 +13,11 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options: CookieOptions }[]) {
+          try {
+        cookiesToSet.forEach(({ name, value, options }) =>
+        cookieStore.set(name, value, options)
+    )
           cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
           response = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) =>
